b4f752f6a1e1dd44e8e4cfbb2054d159b8b77c34
Mike Wilcox <mwilcox56@gmail.com>
2018-07-25 10:27:47 -0400

    cleanup

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,11 +1,6 @@
 import { Query } from './Query'
-import { auth } from './Mutation/auth'
-import { post } from './Mutation/post'
 
 export default {
   Query,
-  Mutation: {
-    ...auth,
-    ...post,
-  },
+  Mutation: {},
 }
