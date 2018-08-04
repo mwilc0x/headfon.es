8d276fe355ded9f5527d9c3c3802dd54eade7cb6
Mike Wilcox <mwilcox56@gmail.com>
2018-08-04 15:59:20 -0400

    mongo environments

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
 export const initDatabase = () => {
-  mongoose.connect(process.env.MONGO_DB_URI);
+  const MONGO_URI = process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_DB_URI_PROD
+    : process.env.MONGO_DB_URI_DEV;
+
+  mongoose.connect(MONGO_URI);
 
   const db = mongoose.connection;
   db.on('error', console.error);
   db.once('open', () => {
-    console.log(`✅   MongoDB connection established! ${process.env.MONGO_DB_URI}`);
+    console.log(`✅   MongoDB connection established!`);
   });
 }
