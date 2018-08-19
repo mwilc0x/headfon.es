71aefe74a1a8ff1db87d9b8fa33b70f4f0dcf8c3
Mike Wilcox <mwilcox56@gmail.com>
2018-08-19 12:13:56 -0400

    handle login failure

--- server/src/middleware/spotify.ts
+++ server/src/middleware/spotify.ts
@@ -2,7 +2,7 @@ import passport from 'passport';
 import scope from '../scope.json';
 const { SCOPE_VALUES } = scope;
 
-const base = passport.authenticate('spotify', { session: false });
+const base = passport.authenticate('spotify', { session: false, failureRedirect: '/' });
 
 const redirect =  (req, res, next) => {
   return passport.authenticate('spotify', <any>{
