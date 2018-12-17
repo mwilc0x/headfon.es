0df95718e2cc6cf210e5e7f855b1976ef37969e2
Mike Wilcox <mwilcox56@gmail.com>
2018-12-17 08:39:24 -0500

    rm HOST variable

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,12 +1,7 @@
-const HOST = process.env.NODE_ENV === 'production' 
-  ? process.env.PROD_HOST 
-  : process.env.DEV_HOST;
-
 const PORT = process.env.NODE_ENV === 'production' 
   ? process.env.PROD_PORT 
   : process.env.DEV_PORT;
 
 export {
-  HOST,
   PORT
 };
